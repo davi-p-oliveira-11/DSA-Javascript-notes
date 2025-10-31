888e63cfe53bc850bf032b7d9400284306cc501d
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-31 17:07:35 -0300

    added sentenceCapitalization.js

---
+++ 01-Arrays/Challenges/sentenceCapitalization.js
@@ -0,0 +1,15 @@
+// 1. Make the string lowercase (toLowerCase method)
+// 2. Convert string to array (split method)
+// 3. Capitalize each word (map method)
+// 4. Convert array back to string (join method)
+
+const capitalize = (str) => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+console.log(capitalize("hallo mars"));
+console.log(capitalize("david waldstein IV"));
